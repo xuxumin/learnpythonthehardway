95bf388c03bcd7bb4d8f6b4fbb9e3135b6595821
xuxumin <xu_xumin@outlook.com>
2018-08-17 17:39:18 -0400

    current text book is based on python2, but i'm using python3.
    add ex13 on vmware ubuntu

--- ex12.py
+++ ex12.py
@@ -1,6 +1,8 @@
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How heavy are you? " )
+
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How heavy are you? " )
 
 print ("So, you're %r old, %r tall and %r heavy."%(
     age, height, weight))
